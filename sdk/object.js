93afc0bc255724a3db3d273c51aefe8527a0cb6c
ChimiDEV <Tim.Walter1996@web.de>
2018-01-13 16:30:05 +0100

    Refactor SDK to ES6 standard

--- sdk/object.js
+++ sdk/object.js
@@ -2,207 +2,217 @@
 © Copyright IBM Corp. 2017
 */
 
-var request = require('request');
+const request = require('request');
 
-var api_key = process.env.API_KEY;
-var knowledge_url = process.env.HUB_URL + 'knowledge/';
+const API_KEY = process.env.API_KEY;
+const KNOWLEDGE_URL = process.env.HUB_URL + 'knowledge/';
 
-function KnowledgeObject(type, attributes, id) {
-  this.type = type;
-  this.attributes = attributes;
-  if (id) {
-    this.id = id;
-  }
-}
-
-// Munges the properties that come from knowledge query into a normal dictionary
-function getAttributes(properties) {
-  var attributes = {};
-  Object.keys(properties).forEach(function (key) {
-    attributes[key] = properties[key][0]['value']
-  });
-  return attributes;
-}
-
-KnowledgeObject.retrieve = function(objectId) {
-  var headers = {
-    'api_key': api_key
-  };
-
-  var options = {
-    url: knowledge_url + 'object/' + objectId,
-    method: 'get',
-    headers: headers
-  };
-
-  return new Promise(function (res, rej) {
-    request(options, function(err, response, body) {
-      if (err || response.statusCode !== 200) {
-        rej(body);
-      } else {
-        var object = JSON.parse(body)['results'][0];
-
-        var type = object['type'];
-
-        var attributes = object['attributes'];
-
-        res(new KnowledgeObject(type, attributes, objectId));
-      }
-    });
-  });
+const headers = {
+  'Content-type': 'application/json',
+  'api_key': API_KEY
 };
 
-KnowledgeObject.prototype.relations = function(type, direction) {
-  var headers = {
-    'api_key': api_key
-  };
-
-  var options = {
-    url: knowledge_url + 'object/' + this.id + '/' + direction,
-    method: 'get',
-    headers: headers
-  };
-
-  if (type) {
-    options['qs'] = {
-      'relation_type': type
-    };
+class KnowledgeObject {
+  /**
+   * 
+   * @param {string} type 
+   * @param {Object} attributes 
+   * @param {string} id 
+   */
+  constructor(type, attributes, id) {
+    this.type = type;
+    this.attributes = attributes;
+    if (id) {
+      this.id = id;
+    }
   }
 
-  return new Promise(function (res, rej) {
-    request(options, function(err, response, body) {
-      if (err || response.statusCode !== 200) {
-        rej(body);
-      } else {
-        var objectData = JSON.parse(body)['results'];
+  /**
+   * 
+   * @param {string} objectId 
+   */
+  static retrieve(objectId) {
+    const options = {
+      url: `${KNOWLEDGE_URL}object/${objectId}`,
+      method: 'GET',
+      headers
+    }
 
-        var objects = [];
-        // Yeah, *datum*.  I'm a nerd and couldn't think of something better.
-        objectData.forEach(function(objectDatum) {
-          var type = objectDatum['type'];
-          var attributes = objectDatum['attributes'];
-          var id = objectDatum['id'];
+    return new Promise((resolve, reject) => {
+      request(options, (err, response, body) => {
+        if (err || response.statusCode !== 200) {
+          return reject(body);
+        }
+
+        const kObject = body.result[0];
+        const type = kObject.type;
+        const attributes = kObject.attributes;
+
+        return resolve(new KnowledgeObject(type, attributes, objectId));
+      });
+    });
+  };
+
+  /**
+   * 
+   * @param {string} type 
+   * @param {string} direction 
+   */
+  relations(type, direction) {
+    let options = {
+      url: `${KNOWLEDGE_URL}obejct/${this.id}/${direction}`,
+      method: 'GET',
+      headers
+    }
+
+    if (type) {
+      options.qs = {
+        relation_type: type
+      };
+    }
+
+    return new Promise((resolve, reject) => {
+      request(options, (err, response, body) => {
+        if (err || response.statusCode !== 200) {
+          return reject(body);
+        }
+
+        const objectsData = body.results;
+        let objects = [];
+        objectsData.forEach(objectData => {
+          const type = objectData.type;
+          const attributes = objectData.attributes;
+          const id = objectData.id;
           objects.push(new KnowledgeObject(type, attributes, id));
         });
-
-        res(objects);
-      }
+        return resolve(objects);
+      });
     });
-  });
-};
-
-KnowledgeObject.prototype.in = function(type) {
-  return this.relations(type, "in");
-};
-
-KnowledgeObject.prototype.out = function(type) {
-  return this.relations(type, "out");
-};
-
-KnowledgeObject.prototype.both = function(type) {
-  return this.relations(type, "both");
-};
-
-KnowledgeObject.prototype.create = function() {
-  if (this.id) {
-    throw 'KnowledgeObject already created -- try update()'
-  }
-  var post_data = {
-    'attributes': this.attributes,
-    'type': this.type
-  };
-  var headers = {
-    'Content-type': 'application/json',
-    'api_key': api_key
   };
 
-  var options = {
-    url: knowledge_url + 'object',
-    method: 'POST',
-    headers: headers,
-    body: JSON.stringify(post_data)
+  /**
+   * 
+   * @param {string} type 
+   */
+  in (type) {
+    return this.relations(type, 'in');
   };
 
-  var self = this;
-  return new Promise(function (res, rej) {
-    request(options, function(err, response, body) {
-      if (err || response.statusCode !== 200) {
-        console.log('Error creating object (status code ' + response.statusCode + ': ' + err + ' ' + body);
-        rej(body);
-      } else {
-        var result = JSON.parse(body)['results'];
-        if (!result){
-          console.log("Error creating object:", body);
-          rej(body);
-        } else {
-          self.id = result[0]['id'];
-          console.log('Saved object with id: ', self.id, 'and type:', self.type);
-          res(body);
+  /**
+   * 
+   * @param {string} type 
+   */
+  out(type) {
+    return this.relations(type, 'out');
+  };
+
+  /**
+   * 
+   * @param {string} type 
+   */
+  both(type) {
+    return this.relations(type, 'both');
+  };
+
+
+  create() {
+    if (this.id) {
+      throw {
+        name: 'AlreadyCreated',
+        message: 'KnowledgeObject already created -- try update()'
+      };
+    }
+
+    const postData = {
+      attributes: this.attributes,
+      type: this.type
+    };
+
+    const options = {
+      url: `${KNOWLEDGE_URL}object`,
+      method: 'POST',
+      headers,
+      json: postData
+    };
+
+    return new Promise((resolve, reject) => {
+      request(options, (err, response, body) => {
+        if (err || response.statusCode !== 200) {
+          console.log(`Error creating object (status code ${response.statusCode}: ${err} ${body}`);
+          return reject(body);
         }
-      }
+
+        const result = body.results;
+        if (!result) {
+          console.log(`error creating object: ${body}`);
+          return reject(body);
+        }
+
+        this.id = result[0].id;
+        console.log(`Saved object with id: ${this.id} and type ${this.type}`);
+        return resolve(body);
+      });
     });
-  });
-};
-
-KnowledgeObject.prototype.update = function() {
-  if (!this.id) {
-    throw 'No id for this object -- try create()';
-  }
-  var post_data = {
-    'attributes': this.attributes,
-    'id': this.id
-  };
-  var headers = {
-    'Content-type': 'application/json',
-    'api_key': api_key
   };
 
-  var options = {
-    url: knowledge_url + 'object/' + this.id,
-    method: 'put',
-    headers: headers,
-    body: JSON.stringify(post_data)
-  };
-  var self = this;
-  return new Promise(function (res, rej) {
-    request(options, function(err, response, body) {
-      if (err || response.statusCode !== 200) {
-        console.log('Error updating object: ' + err + ' ' + body);
-        rej(body);
-      } else {
-        console.log('Updated object with id: ', self.id, 'and type:', self.type);
-        res(body);
-      }
+  update() {
+    if (!this.id) {
+      throw {
+        name: 'NotCreated',
+        message: 'KnowledgeObject has no id -- try create()'
+      };
+    }
+
+    const postData = {
+      attributes: this.attributes,
+      id: this.id
+    };
+
+    const options = {
+      url: `${KNOWLEDGE_URL}object/${this.id}`,
+      method: 'PUT',
+      headers,
+      json: postData
+    }
+
+    return new Promise((resolve, reject) => {
+      request(options, (err, response, body) => {
+        if (err || response.statusCode !== 200) {
+          console.log(`Error updating object (status code ${response.statusCode}: ${err} ${body}`);
+          return reject(body);
+        }
+
+        console.log(`Updated object with id ${this.id} and type ${this.type}`);
+        return resolve(body);
+      });
     });
-  });
-};
-
-KnowledgeObject.prototype.delete = function() {
-  if (!this.id) {
-    throw 'No id for this object -- did you already delete it?';
-  }
-  var headers = {
-    'Content-type': 'application/json',
-    'api_key': api_key
   };
 
-  var options = {
-    url: knowledge_url + 'object/' + this.id,
-    method: 'delete',
-    headers: headers,
-  };
-  var self = this;
-  return new Promise(function (res, rej) {
-    request(options, function(err, response, body) {
-      if (err || response.statusCode !== 200) {
-        console.log('Error deleting object: ' + err + ' ' + body);
-        rej(body);
-      } else {
-        console.log('Object', self.id, 'deleted.');
-        res(body);
-      }
+  delete() {
+    if (!this.id) {
+      throw {
+        name: 'AlreadyDeleted',
+        message: 'No id for this object -- did you already delte it?'
+      };
+    }
+
+    const options = {
+      url: `${KNOWLEDGE_URL}object/${this.id}`,
+      method: 'DELETE',
+      headers
+    };
+
+    return new Promise((resolve, reject) => {
+      request(options, (err, response, body) => {
+        if (err || response.statusCode !== 200) {
+          console.log(`Error deleting object: ${err} ${body}`);
+          return reject(body);
+        }
+        console.log(`Object ${this.id}(${this.type}) deleted`);
+        return resolve(body);
+      });
     });
-  });
-};
+  };
+}
 
 module.exports = KnowledgeObject;
